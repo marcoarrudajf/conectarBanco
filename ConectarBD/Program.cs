8e46f4e2f27253518dc3edffcb4f34bcd1891017
Primeiro <marcoarruda.dev@gmail.com>
2024-02-04 10:18:48 -0300

    [primeiro commit]

---
+++ ConectarBD/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConectarBD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Começando a criar");
+        }
+    }
+}
